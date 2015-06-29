4394b4fcf1a94f5fad240d4990f404682b8b8bdd
Manuel Sánchez <Manasan93@gmail.com>
2015-06-29 10:49:39 -0600

    Update gameMain.c

--- gameMain.c
+++ gameMain.c
@@ -1,29 +1,29 @@
 #include "Libs/mainLib.h"
-
+ 
 int main(int argc, char const *argv[]){
-
-	bool isRunning = true;
-	
-	sdlStartup(); //Startup SDL Subsystems
-	sdlMediaStartup(); //Startup SDL Media Resources
-	allocateMatrix(&matrix); //Allocate Memory Space For The Matrix
-	fillMatrix(matrix); //Fill Matrix with identifiers for Egg Types
-	drawEggs(renderer,matrix); //Draw Eggs on renderer
-	drawAntiEgg();
-	showMatrix(matrix); //Show Matrix on Console, for testing purposes
-
-	while(isRunning){
-		if( SDL_PollEvent(&events) != 0 ){
-			if(events.type == SDL_QUIT){
-				isRunning = false;
-			}
-			moveAntiEggEvent();
-		}
-		if(pause == false){
-			SDL_RenderPresent(renderer);
-		}
-	}
-
-	closeALL();
-	return 0;
+ 
+    bool isRunning = true;
+     
+    sdlStartup(); //Startup SDL Subsystems
+    sdlMediaStartup(); //Startup SDL Media Resources
+    allocateMatrix(&matrix); //Allocate Memory Space For The Matrix
+    fillMatrix(matrix); //Fill Matrix with identifiers for Egg Types
+    drawEggs(renderer,matrix); //Draw Eggs on renderer
+    drawBuster();
+    showMatrix(matrix); //Show Matrix on Console, for testing purposes
+ 
+    while(isRunning){
+        if( SDL_PollEvent(&events) != 0 ){
+            if(events.type == SDL_QUIT){
+                isRunning = false;
+            }
+            moveBuster();
+        }
+        if(pause == false){
+            SDL_RenderPresent(renderer);
+        }
+    }
+ 
+    closeALL();
+    return 0;
 }

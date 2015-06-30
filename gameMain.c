2aaf7ec7f600ab043bca9f8b509d50c434f56241
Manuel Sánchez <Manasan93@gmail.com>
2015-06-30 12:58:23 -0600

    Working main

--- gameMain.c
+++ gameMain.c
@@ -1,29 +1,40 @@
 #include "Libs/mainLib.h"
- 
+
+
 int main(int argc, char const *argv[]){
- 
+ 	
     bool isRunning = true;
-     
+    Uint32 start;
+
     sdlStartup(); //Startup SDL Subsystems
     sdlMediaStartup(); //Startup SDL Media Resources
     allocateMatrix(&matrix); //Allocate Memory Space For The Matrix
     fillMatrix(matrix); //Fill Matrix with identifiers for Egg Types
     drawEggs(renderer,matrix); //Draw Eggs on renderer
-    drawBuster();
+    drawBuster(); //Draw The Egg Buster on screen center
     showMatrix(matrix); //Show Matrix on Console, for testing purposes
  
     while(isRunning){
+    	start = SDL_GetTicks();
         if( SDL_PollEvent(&events) != 0 ){
-            if(events.type == SDL_QUIT){
-                isRunning = false;
-            }
-            moveBuster();
+        	switch(events.type){
+
+        		case SDL_QUIT:
+        			isRunning = false;
+        			break;
+
+        		default:
+        			break;
+        	}
+        	moveBuster(); 
         }
-        if(pause == false){
-            SDL_RenderPresent(renderer);
+
+        //SDL_RenderPresent(renderer);
+        if(TICKS_PER_FRAME > SDL_GetTicks() - start){
+        	SDL_Delay(TICKS_PER_FRAME - (SDL_GetTicks() - start));
         }
+        
     }
- 
     closeALL();
     return 0;
 }
